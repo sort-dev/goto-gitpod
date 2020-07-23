df73c152bcc0490c91011283de5391a747ce568d
Jayson Minard <jayson.minard@gmail.com>
2020-07-23 17:34:40 -0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,2 +1,2 @@
 export|grep ASSESSMENT_REPO
-git clone --bare https://github.com/$ASSESSMENT_REPO /workspace/assessment/.git"
+git clone --bare https://github.com/$ASSESSMENT_REPO /workspace/assessment/.git
