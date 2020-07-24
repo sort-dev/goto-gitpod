3865ddd6ee828dbdc9d0ce79d4ded79097acb43a
Jayson Minard <jayson.minard@gmail.com>
2020-07-23 18:38:50 -0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,9 +1,2 @@
-git config --global credential.helper
-echo "protocol=https" > credcheck.txt
-echo "host=github.com" >> credcheck.txt
-echo "" >> credcheck.txt
-echo "" >> credcheck.txt
-cat credcheck.txt | git credential fill
-export|grep ASSESSMENT_REPO
-sleep 2
+sleep 1
 git clone --bare https://github.com/$ASSESSMENT_REPO /workspace/assessment/.git
